bb0cbc67821faf9dfe0fd8336268b3db8e5388f0
Arthur <105012211+arthursousa10@users.noreply.github.com>
2023-05-08 16:11:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const botaoLancarDados = document.getElementById("botao-lancar-dados");
+const resultadoLancamento = document.getElementById("resultado-lancamento");
+
+function lancarDados() {
+    let resultado1 = Math.floor(Math.random()* 6) + 1;
+    let resultado2 = Math.floor(Math.random()* 6) + 1;
+    let SomaResultados = resultado1 + resultado2;
+    resultadoLancamento.innerHTML = `
+    <p>O resutado do lançamento dos dados foi:</p>
+    <div>
+      <img src="dado ${resultado1}.png" alt="Dado${resultado1}">
+      <img src="dado ${resultado2}.png" alt="Dado${resultado2}">
+    </div>
+    <p> A soma dos resultados é: ${SomaResultados}</p>
+    `;
+}
+
+botaoLancarDados.addEventListener("click", lancarDados);
